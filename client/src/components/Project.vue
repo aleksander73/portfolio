112777d94186791c43c3782c6d185c63d60a377b
aleksander73 <olekdobkowski@gmail.com>
2021-02-03 14:08:17 +0100

    Add technology component and integrate into project

--- client/src/components/Project.vue
+++ client/src/components/Project.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>{{name}}</h1>
+        <Technology v-for="(technology, index) in technologies" :key=index :name=technology.name :icon=technology.icon />
         <p>{{description}}</p>
         <img v-for="(image, index) in images" :key="index" :src="relativePath(image)" alt="image">
         <iframe width='560' height='315' :src=ytVideoSrc frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen />
@@ -11,9 +12,13 @@
 
 <script>
 import Storage from '../Storage';
+import Technology from './Technology.vue';
 
 export default {
     name: 'Project',
+    components: {
+        Technology
+    },
     data() {
         return {
             technologies: []
