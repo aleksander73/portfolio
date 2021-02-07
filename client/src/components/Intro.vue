a811fc65c155ef0c7322bcb2d714f8d33f2e3cc5
aleksander73 <olekdobkowski@gmail.com>
2021-02-07 01:05:59 +0100

    Add intro component

---
+++ client/src/components/Intro.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="intro">
+        <div class="top">
+            <img src="../../assets/social/profile.png">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint soluta aspernatur, maiores neque beatae quibusdam consequuntur quas fuga voluptatem distinctio libero dicta incidunt molestias magnam officia porro blanditiis veritatis!</p>
+        </div>
+        <a href="#projects-start">
+            <button class="button">My projects</button>
+        </a>
+    </div>
+</template>
+
+<style scoped>
+#intro {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 75%;
+    height: 30vh;
+    margin-bottom: 15vh;
+}
+
+.top > img {
+    width: 35vh;
+    height: 35vh;
+    border-radius: 50%;
+}
+
+.top p {
+    width: 65vh;
+    font-size: 1.5em;
+    text-align: center;
+    line-height: 1.5em;
+    color: #dadada;
+}
+
+.button {
+    font-family: 'Courier New', Courier, monospace;
+    border: 0;
+    background-color: rgb(26, 137, 228);
+    width: 35vh;
+    height: 10vh;
+    font-size: 1.5em;
+    text-decoration: none;
+    cursor: pointer;
+    text-transform: uppercase;
+}
+</style>
+
+<script>
+export default {
+    name: 'Intro'
+}
+</script>
