087b64f0d130d0f6945b06eae42501fa1fe62614
aleksander73 <olekdobkowski@gmail.com>
2021-05-04 12:40:06 +0200

    Redirect unknown paths to home page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,19 +3,20 @@ import { ControlPanel, Home, Login } from '../views';
 const router = {
     routes: [
         {
-            name: 'home',
             path: '/',
             component: Home
         },
         {
-            name: 'login',
             path: '/admin',
             component: Login
         },
         {
-            name: 'control-panel',
             path: '/control-panel',
             component: ControlPanel
+        },
+        {
+            path: '*',
+            redirect: '/'
         }
     ]
 };
