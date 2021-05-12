182f53d4f928eec2642e2aecbe304498286f83e3
aleksander73 <olekdobkowski@gmail.com>
2021-05-12 14:50:11 +0200

    Add technology service to index

--- server/services/index.js
+++ server/services/index.js
@@ -1,9 +1,11 @@
 const cryptographyService = require('./CryptographyService');
 const projectService = require('./ProjectService');
+const technologyService = require('./TechnologyService');
 const userService = require('./UserService');
 
 module.exports = {
     cryptographyService,
     projectService,
+    technologyService,
     userService
 };
