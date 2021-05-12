e126a0e961c965afcf12698e8abc8d10ab7cb8dc
aleksander73 <olekdobkowski@gmail.com>
2021-05-12 15:10:55 +0200

    Setup add technology endpoint

--- server/api/TechnologyRouter.js
+++ server/api/TechnologyRouter.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const { Database } = require('../database');
+const { technologyService } = require('../services');
+const { upload } = require('../middleware');
 
 const router = express.Router();
+
 router.get('/', async (req, res) => {
-    const technologies = await Database.getInstance().getCollection('technologies');
+    const technologies = await technologyService.getTechnologies();
     res.send(technologies);
+});
+
+router.post('/add', upload.single('icon'), async (req, res) => {
+    try {
+        const { tag, name } = req.body;
+        const icon = req.file.filename;
+        await technologyService.addTechnology(tag, name, icon);
+        res.sendStatus(200);
+    } catch(error) {
+        res.status(400).send(error.message);
+    }
 });
 
 module.exports = router;
