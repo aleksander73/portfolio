7c5d541d49d00ddd61cad48c8dce0ecff29e2c33
aleksander73 <olekdobkowski@gmail.com>
2021-05-13 03:34:47 +0200

    Add link to index

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,10 @@
+const Link = require('./Link');
 const Project = require('./Project');
 const Technology = require('./Technology');
 const User = require('./User');
 
 module.exports = {
+    Link,
     Project,
     Technology,
     User
