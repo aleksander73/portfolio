e9c583f5e766cadaa30dd6aeb117265413ba8d8c
aleksander73 <olekdobkowski@gmail.com>
2021-05-15 02:13:08 +0200

    Remove deleted project pictures from the server

--- server/services/ProjectService.js
+++ server/services/ProjectService.js
@@ -1,5 +1,6 @@
 const { Project } = require('../models');
 const { Database } = require('../database');
+const fs =require('fs');
 
 class ProjectService {
     async getProjects(filter) {
@@ -12,6 +13,7 @@ class ProjectService {
     }
 
     async editProject(_id, name, description, features, githubRepo, technologies, technologyTag, allPictures, deletedPictures, uploadedPictures, ytVideoId, score) {
+        deletedPictures.forEach(picture => fs.unlinkSync(`${__dirname}/../uploads/${picture}`));
         let pictures = allPictures.filter(x => !deletedPictures.includes(x));
         uploadedPictures.forEach(x => pictures.push(x));
         Database.getInstance().updateDocument('projects', _id, {
