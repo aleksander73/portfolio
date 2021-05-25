41ec575bf88c73158e7272793b211e9d3085e0c8
aleksander73 <olekdobkowski@gmail.com>
2021-05-25 20:55:29 +0200

    Support overflow-y in home page index

--- frontend/src/components/Index.vue
+++ frontend/src/components/Index.vue
@@ -2,7 +2,7 @@
     <div class="index-container">
         <div class="index">
             <div class="items-container center-y">
-                <div class="index-item" v-for="project in projects" :key="project.githubRepo" @click=onIndexItemClick(project)>{{project.name}}</div>
+                <div class="index-item" v-for="project in projects" :key="project._id" @click=onIndexItemClick(project)>{{project.name}}</div>
             </div>
         </div>
     </div>
@@ -11,27 +11,31 @@
 <style scoped>
 .index-container {
     background-color: #0f0f0fdb;
-    width: 10%;
+    width: 20%;
     margin: 0 auto;
-    padding: 0 5em;
     text-align: center;
 }
 
 .index {
     position: sticky;
     top: 0;
-    padding-top: 3em;
+    padding: 3em 0;
+    width: 75%;
+    margin: 0 auto;
 }
 
 .items-container {
     flex-direction: column;
+    overflow-y: auto;
+    overflow-x: hidden;
+    max-height: calc(100vh - 6em);
 }
 
 .index-item {
     margin-bottom: 0.75em;
     padding: 0.3em;
     font-size: 1.25em;
-    width: 100%;
+    width: 80%;
     border: 1px solid #e2e2e2;
     text-align: center;
     cursor: pointer;
